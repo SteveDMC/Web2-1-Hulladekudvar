4b5b7e528f1e2879938bfd00bbda230739678072
Bérczy Bálint <berczy.balint@gmail.com>
2023-11-06 18:54:18 +0100

    DB létrehozása importáláshoz

--- hulladekudvar.sql
+++ hulladekudvar.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Adatbázis: `hulladekudvar`
 --
 
+CREATE DATABASE IF NOT EXISTS `hulladekudvar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `hulladekudvar`;
+
 -- --------------------------------------------------------
 
 --
