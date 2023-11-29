adb0a82bbb9702dd29e181b5fe0c4eca66511674
SteveDMC <dmc120@gmail.com>
2023-11-29 14:50:22 +0100

    Kezdőlap frissítés

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,8 +1,6 @@
 <div class="nyito">
     <h2>
         <br>Üdvözöljük Hulladékudvarunk weboldalán!<br>
-        <br>Itt minden kérdésére választ talál a hulladék kezeléssel kapcsolatban!<br>
-        <br>Jó böngészést kívánunk!<br>
     </h2>
 </div>
 <div class="doboz">
